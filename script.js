153ccecf9207aea914abd09253ad8fed9a812d3c
Kar-ho <k-h_lee@hotmail.co.uk>
2023-05-22 10:33:13 +0100

    added operate function

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-const add = function(a, b) {
+function add(a, b) {
 	return a + b
 };
 
-const subtract = function(a, b) {
+function subtract(a, b) {
 	return a - b
 };
 
-const divide = function(a, b) {
+function multiply(a, b) {
+  return a * b;
+};
+
+function divide(a, b) {
   return a / b
 };
 
-const multiply = function(a, b) {
-  return a * b;
-};
+function operate(a, b, operator) {
+    switch (operator) {
+      case '+':
+        return add(a, b);
+      case '-':
+        return subtract(a, b);
+      case '*':
+        return multiply(a, b);
+      case '/':
+        return divide(a, b);
+    }
+}
