c434c508959a57c229bf0d73e6d3f5925a03485d
karho.lee <k-h_lee@hotmail.co.uk>
2024-02-10 14:03:47 +0000

    fix decimal bug and add styling

--- script.js
+++ script.js
@@ -1,103 +1,110 @@
 let a;
 let b;
 let result = 0;
-let mathSign = '';
+let mathSign = "";
 let displayValue = [0];
-let display = document.querySelector('.display');
-
-function add(a, b) {
-	return a + b
-};
-
-function subtract(a, b) {
-	return a - b
-};
-
-function multiply(a, b) {
-  return a * b;
-};
-
-function divide(a, b) {
-  return a / b
-};
-
-function operate(a, b, operator) {
-    switch (operator) {
-      case '+':
-        return add(a, b);
-      case '-':
-        return subtract(a, b);
-      case '*':
-        return multiply(a, b);
-      case '/':
-        return divide(a, b);
-    }
-}
+let display = document.querySelector(".display");
 
 function calcDisplay() {
-  let btn = document.querySelectorAll('.num');
-  btn.forEach((number) => number.addEventListener('click', function() {
-    displayValue.push(this.textContent);
-    display.textContent = parseFloat(displayValue.join(''));
-  }));
+  let btn = document.querySelectorAll(".num");
+  btn.forEach((number) =>
+    number.addEventListener("click", function () {
+      displayValue.push(this.textContent);
+      display.textContent = parseFloat(displayValue.join(""));
+    })
+  );
   reset();
   operator();
   equals();
 }
 
-function operator() {
-  let operator = document.querySelectorAll('.operator')
-  operator.forEach((op) => op.addEventListener('click', function() {
-    if (a == undefined) {
-      a = parseFloat(displayValue.join(''));
-      displayValue = [0];
-      mathSign = this.textContent;
-    } else {
-      b = parseFloat(displayValue.join(''));
-      result = operate(a, b, mathSign);
-      display.textContent = result;
-      a = result;
-      mathSign = this.textContent;
-      displayValue = [0];
-      console.log(`a: ${a}`)
-      console.log(`b: ${b}`)
-      console.log(result)
-      console.log(mathSign)
-    }
-  }))
-}
-
-function equals() {
-  let equal = document.querySelector('.equal')
-  equal.addEventListener('click', function() {
-    if (a == undefined) {
-      display.textContent = parseFloat(displayValue.join(''))
-    } else {
-      b = parseFloat(displayValue.join(''));
-      result = operate(a, b, mathSign)
-      display.textContent = result
-      a = result;
-      if (mathSign == '*') {
-        displayValue = [1]
-      } else {
-        displayValue = [0]
-      }
-      console.log(`a: ${a}`)
-      console.log(`b: ${b}`)
-      console.log(mathSign)
-    }
-  })
-}
-
 function reset() {
-  let reset = document.querySelector('#reset')
-  reset.addEventListener('click', function() {
+  let reset = document.querySelector("#reset");
+  reset.addEventListener("click", function () {
     displayValue = [0];
     display.textContent = 0;
     result = 0;
     a = undefined;
     b = undefined;
-  })
+  });
 }
 
-calcDisplay()
+function operator() {
+  let operator = document.querySelectorAll(".operator");
+  operator.forEach((op) =>
+    op.addEventListener("click", function () {
+      mathSign = this.textContent;
+      console.log(mathSign);
+      if (a == undefined) {
+        a = parseFloat(displayValue.join(""));
+        displayValue = [0];
+      } else {
+        if (mathSign === this.textContent) {
+          return;
+        }
+
+        b = parseFloat(displayValue.join(""));
+        result = operate(a, b, mathSign);
+        display.textContent = result;
+        a = result;
+        displayValue = [0];
+      }
+    })
+  );
+}
+
+function equals() {
+  let equal = document.querySelector(".equal");
+  equal.addEventListener("click", function () {
+    if (a == undefined) {
+      display.textContent = parseFloat(displayValue.join(""));
+    } else {
+      b = parseFloat(displayValue.join(""));
+      result = operate(a, b, mathSign);
+
+      if (Number.isInteger(result)) {
+        display.textContent = result;
+      } else {
+        display.textContent = result.toFixed(10).replace(/\.?0+$/, "");
+      }
+
+      a = result;
+      if (mathSign == "*") {
+        displayValue = [1];
+      } else {
+        displayValue = [0];
+      }
+    }
+  });
+}
+
+function operate(a, b, operator) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+  }
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+calcDisplay();
